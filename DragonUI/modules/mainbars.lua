255cdd7d9cbd7911ebe686d1d1d978b1b9daead4
Jeff. <jeffreysfu@gmail.com>
2026-06-08 21:47:59 -0400

    fix(actionbars): add stealth bar switching for CoA custom classes
    PROPHET, RANGER, REAPER, and SPIRITMAGE now correctly switch the
    primary action bar to the stealth page when entering stealth, matching
    the ElvUI Ascension fork approach.
    
    CoA custom classes with stealth use the [stealth] macro condition
    instead of the vanilla [bonusbar:N] system, so they need explicit
    class entries in the page driver condition table.

--- DragonUI/modules/mainbars.lua
+++ DragonUI/modules/mainbars.lua
@@ -134,11 +134,16 @@ local function IsModuleEnabled()
 end
 
 local mainBarPageByClass = {
-    DRUID = '[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 7; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;',
-    WARRIOR = '[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;',
-    PRIEST = '[bonusbar:1] 7;',
-    ROGUE = '[bonusbar:1] 7; [bonusbar:2] 8;',
-    DEFAULT = '[bonusbar:5] 11; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;'
+  DRUID = '[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 7; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;',
+  WARRIOR = '[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;',
+  PRIEST = '[bonusbar:1] 7;',
+  ROGUE = '[bonusbar:1] 7; [bonusbar:2] 8;',
+  -- CoA custom classes with stealth (use [stealth] condition, not bonusbar)
+  PROPHET = '[stealth] 7; [nostealth] 1;',
+  RANGER = '[stealth] 7; [nostealth] 1;',
+  REAPER = '[stealth] 7; [nostealth] 1;',
+  SPIRITMAGE = '[stealth] 7; [nostealth] 1;',
+  DEFAULT = '[bonusbar:5] 11; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;'
 }
 
 local function GetMainBarPageCondition()
